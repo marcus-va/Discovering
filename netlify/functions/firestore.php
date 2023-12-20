e2c0efcc5db31ec1131df4eff1a40d7bf6bd5bdf
marcus-va <109010363+marcus-va@users.noreply.github.com>
2023-12-20 19:59:47 -0300

    Add files via upload

---
+++ netlify/functions/firestore.php
@@ -0,0 +1,39 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+use Google\Cloud\Firestore\FirestoreClient;
+
+class Firestore
+{
+    private $db;
+
+
+    public function __construct()
+    {
+        $this->db = new FirestoreClient([
+            'projectId' => getenv('FIREBASE_PROJECT_ID'),
+            'keyFilePath' => base64_decode(getenv('GOOGLE_APPLICATION_CREDENTIALS')),
+        ]);
+    }
+
+    public function getCustomers()
+    {
+        $customersRef = $this->db->collection('customers');
+        $snapshot = $customersRef->documents();
+
+        $customers = [];
+
+        foreach ($snapshot as $document) {
+            $customers[] = $document->data();
+        }
+
+        return $customers;
+    }
+
+    public function addCustomer($data)
+    {
+        $customersRef = $this->db->collection('customers');
+        $customersRef->add($data);
+    }
+}
