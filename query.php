b1cad8f481df9cb7b2b9b8cfe79ef63de1f4f5bc
marcus-va <109010363+marcus-va@users.noreply.github.com>
2023-12-20 18:24:41 -0300

    Update query.php

--- query.php
+++ query.php
@@ -1,9 +1,25 @@
 <?php
 
+// Importa a biblioteca do Cloud Firestore
+require_once "vendor/autoload.php";
+
+// Cria uma instância do cliente do Firestore
 $firestore = new Google\Cloud\Firestore\FirestoreClient();
 
+// Obtém a coleção "clientes"
 $colecao = $firestore->collection("clientes");
 
-$documentos = $colecao->documents();
+// Retorna um array de objetos que representam os clientes
+function queryClientes() {
 
-return $documentos;
+  // Cria uma consulta
+  $consulta = $colecao->where("nome", ">=", "");
+
+  // Executa a consulta
+  $documentos = $consulta->documents();
+
+  // Retorna um array de objetos que representam os clientes
+  return $documentos;
+}
+
+?>
