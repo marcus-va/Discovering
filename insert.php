bec8feb6b7de9ed5078857513b8f0b1fa43e7974
marcus-va <109010363+marcus-va@users.noreply.github.com>
2023-12-20 18:53:22 -0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,7 +4,13 @@
 require_once "vendor/autoload.php";
 
 // Cria uma instância do cliente do Firestore
-$firestore = new Google\Cloud\Firestore\FirestoreClient();
+$firestore = new Google\Cloud\Firestore\FirestoreClient([
+  "projectId" => $_ENV["FIREBASE_PROJECT_ID"],
+  "clientId" => $_ENV["FIREBASE_CLIENT_ID"],
+  "authUri" => $_ENV["FIREBASE_AUTH_URI"],
+  "tokenUri" => $_ENV["FIREBASE_TOKEN_URI"],
+  "authProviderX509CertUrl" => $_ENV["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
+]);
 
 // Obtém a coleção "clientes"
 $colecao = $firestore->collection("clientes");
