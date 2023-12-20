50d2c7ed07dd03b77541540c68066eb9d3462606
marcus-va <109010363+marcus-va@users.noreply.github.com>
2023-12-20 18:24:27 -0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,14 +1,30 @@
 <?php
 
-$cliente = [
-  "nome" => $_POST["nome"],
-  "email" => $_POST["email"],
-  "telefone" => $_POST["telefone"],
-  "endereco" => $_POST["endereco"],
-];
+// Importa a biblioteca do Cloud Firestore
+require_once "vendor/autoload.php";
 
+// Cria uma instância do cliente do Firestore
 $firestore = new Google\Cloud\Firestore\FirestoreClient();
 
+// Obtém a coleção "clientes"
 $colecao = $firestore->collection("clientes");
 
-$colecao->addDocument($cliente);
+// Insere um novo documento na coleção
+function insert($cliente) {
+
+  // Cria um novo documento
+  $documento = $colecao->document();
+
+  // Adiciona os dados do cliente ao documento
+  $documento->set([
+    "nome" => $cliente["nome"],
+    "email" => $cliente["email"],
+    "telefone" => $cliente["telefone"],
+    "endereco" => $cliente["endereco"],
+  ]);
+
+  // Salva o documento
+  $documento->save();
+}
+
+?>
